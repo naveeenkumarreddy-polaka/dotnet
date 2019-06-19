e0572202a6844e578f86f169f17f23529feea8e4
naveeenkumarreddy-polaka <40000751+naveeenkumarreddy-polaka@users.noreply.github.com>
2019-06-19 16:16:29 +0530

    Changing assembly version

--- DancingGoat/Properties/AssemblyInfo.cs
+++ DancingGoat/Properties/AssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Kentico Cloud sample applications")]
 [assembly: ComVisible(false)]
 [assembly: Guid("ede989ba-1498-4a94-956c-c39969a3ebec")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
